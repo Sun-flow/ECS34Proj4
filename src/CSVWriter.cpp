a0d3d60a534a999530735b041b8d76a67ae784c8
Jacie Mae <jcmoninghoff@gmail.com>
2019-11-13 15:45:43 -0800

    Remove Prints

--- src/CSVWriter.cpp
+++ src/CSVWriter.cpp
@@ -8,13 +8,13 @@ CCSVWriter::CCSVWriter(std::ostream &ou):outStream(ou){
 CCSVWriter::~CCSVWriter(){
 
 }
-
+//Take in vector of strings and put quotes around all of the entries ini a copy vector
 bool CCSVWriter::WriteRow(const std::vector< std::string > &row){
-    std::cout << "Writer cout-put: ";
     for(auto str : row){
         outStream << str << ", ";
-        std::cout << str << ", ";
     }
     std::cout << std::endl;
     return true;
 }
+
+//Carriage return new line is a single line
