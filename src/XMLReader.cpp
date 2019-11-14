1dda7df35d0e6ca09145210d3a225e5fef62ae49
Jacie Mae <jcmoninghoff@gmail.com>
2019-11-13 17:25:49 -0800

    Test Prints

--- src/XMLReader.cpp
+++ src/XMLReader.cpp
@@ -1,7 +1,8 @@
 #include "XMLReader.h"
+#include <iostream>
 
 CXMLReader::CXMLReader(std::istream &is) : DInput(is){
-    DParser = XML_ParserCreate(NULL);
+    DParser = XML_ParserCreate(0);
 
     //set handlers/callback functions (Chardata, startelement, endelement)
     XML_SetElementHandler(DParser, StartElemHandler, EndElemHandler);
@@ -13,36 +14,53 @@ CXMLReader::~CXMLReader(){
 }
 
 void CXMLReader::CharDataHandler(void *userData, const XML_Char *s, int len){
-    auto Data = static_cast<CXMLReader *>(userData);
+    auto Reader = static_cast<CXMLReader *>(userData);
     std::string(s, len);
 }
 
 void CXMLReader::StartElemHandler(void *userData, const XML_Char *name, const XML_Char **atts){
     auto Reader = static_cast<CXMLReader *>(userData);
     SXMLEntity Entity;
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     Entity.DType = SXMLEntity::EType::StartElement;
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     Entity.DNameData = static_cast<const char *>(name); 
-    Entity.DAttributes.push_back(atts);
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
+    Entity.SetAttribute(name, atts[0]);
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
+    std::cout << "Name: " << Entity.DNameData << std::endl;
+    Reader->BufferedEntity.push_back(Entity);
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
+
 }
 
 void CXMLReader::EndElemHandler(void *userData, const XML_Char *name){
     auto Reader = static_cast<CXMLReader *>(userData);
     SXMLEntity Entity;
     Entity.DType = SXMLEntity::EType::EndElement;
-    Entity.DNameData = static_cast<const char* >(name); 
+    Entity.DNameData = static_cast< const char* >(name); 
 
 }
 
 bool CXMLReader::End(){
-
+    if(!DInput.eof()){
+		DInput.peek();
+	}
+    return DInput.eof();
 }
 
 bool CXMLReader::ReadEntity(SXMLEntity &entity, bool skipcdata){
     char Buffer[1024];
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     DInput.read(Buffer, sizeof(Buffer));
+    std::cout << __FILE__<< "@ " << __LINE__ << std::endl;
     XML_Parse(DParser, Buffer, DInput.gcount(), DInput.eof());
+    std::cout << __FILE__<< "@ " << __LINE__ << std::endl;
 
-    entity = BufferedEntity.front();
-    BufferedEntity.pop_front();
-
+    //std::cout << "Name: " << BufferedEntity.front().DNameData << std::endl;
+    //entity = BufferedEntity.front();
+    std::cout << __FILE__<< "@ " << __LINE__ << std::endl;
+    //BufferedEntity.pop_front();
+    std::cout << __FILE__<< "@ " << __LINE__ << std::endl;
+    return true;
 }
--- src/testxml.cpp
+++ src/testxml.cpp
@@ -17,12 +17,16 @@ TEST(XMLReader, EmptyTest){
 TEST(XMLReader, SimpleTest){
     std::stringstream Input("<tag><other></other></tag>");
     CXMLReader Reader(Input);
-
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     SXMLEntity Entity;
 
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     EXPECT_TRUE(Reader.ReadEntity(Entity));
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     EXPECT_EQ(Entity.DType, SXMLEntity::EType::StartElement);
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     EXPECT_EQ(Entity.DNameData, "tag");
+    std::cout << __FILE__ << "@ " << __LINE__ << std::endl;
     /*EXPECT_TRUE(Reader.ReadEntity(Entity));
     EXPECT_EQ(Entity.DType, SXMLEntity::EType::StartElement);
     EXPECT_EQ(Entity.DNameData, "other");
