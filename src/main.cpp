3a69fcbe720d7bb4697cffc9066fbfea1ec78d6e
Jacie Mae <jcmoninghoff@gmail.com>
2019-11-18 15:07:09 -0800

    Do Math About It

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,31 @@
 #include <iostream>
 #include <map>
 #include <iterator>
+#include <queue>
+
+struct Gender{
+    int male = 0;
+    int female = 0;
+}
+
+struct RollingAvg{
+    int totals[3];
+    int index = 0;
+    int Avg;
+}
+
+void printData(std::string name, std::string likelySex, double sexProb, int highYear){
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Likely Sex: " << likelySex << " (" << sexProb << "%)" << std::endl;
+    std::cout << "Year: " << highYear << std::endl;
+}
 
 int main(){
 
     std::cout << "MAIN IS HERE" << std::endl;
 
-    
-        
-    CXMLReader XMLReader(inputXML); //Get manifest, needs update on file input
-
-    struct Gender{
-        int male = 0;
-        int female = 0;
-    }
+    std::ifstream Input("proj4data/smallmanifest.xml");
+    CXMLReader XMLReader(Input); //Get manifest, needs update on file input
 
     std::map<std::string , std::map< std::map< std::string , std::map< int , Gender > Names;
     
@@ -46,11 +58,67 @@ int main(){
                 Names[inName][inCountry][inYear].male += inData;
             }
             else if(inData == "F"){
-                Names[inName].second[inCountry].second[inYear].female += inData;
+                Names[inName][inCountry][inYear].female += inData;
             }
         }
     }
-    
+
+    bool end = false;
+    do{
+        std::string inName;
+        std::cout << "Enter Name>>";
+        std::cin >> inName;
+
+        auto countryIter = Names.find(inName)->second.begin();
+        double sexProb;
+        std::string likelySex;
+        RollingAvg likelyYear;
+
+        RollingAvg avg;
+        
+        
+        while(!countryIter.end()){ //Increment country
+            
+            int males = 0;
+            int females = 0;
+            auto yearIter = countryIter->second.begin();
+            while(!yearIter.end()){
+                males += yearIter.male;
+                females += yearIter.female;
+                
+                avg.totals[avg.index%3] = males + females;
+                avg.index++;
+                double tempAvg = 0;
+                for(int i = 0; i < 3; i++){
+                    tempAvg += avg.total[i]; 
+                }
+                if(tempAvg >= avg.Avg){
+                    avg.Avg = tempAvg;
+                    avg.MidYear = yearIter.first - 1;
+                }
+                likelyYear = avg.MidYear;
+
+                yearIter++;
+            }
+
+            if(males > females){
+                likelySex = "M";
+                sexProb = males/(males + females);
+            }
+            else if(females > males){
+                likelySex = "F";
+                sexProb = females/(males + females);
+
+            }
+            else{
+                likelySex = "M/F";
+                sexProb = 50
+            }
+
+            countryIter++;
+        }
+        printData(inName, likelySex, sexProb, likelyYear)
+    }while(!end);
     //Do math about it
 
     //Use input stream to have user give name, search and return math
