f4c9e830c8dd9dfd23beeb68a89a19c6cd0c0294
Jacie Mae <jcmoninghoff@gmail.com>
2019-11-18 16:13:31 -0800

    Bugfixing

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "XMLWriter.h"
 #include "XMLEntity.h"
 #include <iostream>
+#include <fstream>
 #include <map>
 #include <iterator>
 #include <queue>
@@ -11,13 +12,13 @@
 struct Gender{
     int male = 0;
     int female = 0;
-}
+};
 
 struct RollingAvg{
     int totals[3];
     int index = 0;
     int Avg;
-}
+};
 
 void printData(std::string name, std::string likelySex, double sexProb, int highYear){
     std::cout << "Name: " << name << std::endl;
@@ -29,13 +30,13 @@ int main(){
 
     std::cout << "MAIN IS HERE" << std::endl;
 
-    std::ifstream Input("proj4data/smallmanifest.xml");
+    std::ifstream Input("proj4data/smallmanifest.xml", std::ifstream::in);
     CXMLReader XMLReader(Input); //Get manifest, needs update on file input
 
-    std::map<std::string , std::map< std::map< std::string , std::map< int , Gender > Names;
+    std::map<std::string , std::map< std::string , std::map< int , Gender > > > Names;
     
 
-    while(!XMLReader.end()){//Read in XML data
+    while(!XMLReader.End()){//Read in XML data
         
         SXMLEntity entity;
         XMLReader.ReadEntity(entity); //Take in first entity
@@ -43,26 +44,28 @@ int main(){
         //Set up necessary stuff using attributes read in
         std::string inCountry = entity.AttributeValue("Country");
         std::string inYear = entity.AttributeValue("Year");
-        CCSVReader CSVReader(entity.AttributeValue("FILENAME"));
 
-        std::vector<std::string> row;
+        std::ifstream Input(entity.AttributeValue("FILENAME"), std::ifstream::in);
+        CCSVReader CSVReader(Input);
 
-        while(!CSVReader.end()){
+        std::vector< std::string > row;
 
-            CSVReader.ReadRow(&row); 
+        while(!CSVReader.End()){
+
+            CSVReader.ReadRow(row); 
 
             std::string inName = row[0];
-            std::string inGender = row[1]
-            int inData = row[2];
+            std::string inGender = row[1];
+            int inData = std::stoi(row[2]);
             if(inGender == "M"){
-                Names[inName][inCountry][inYear].male += inData;
+                Names[inName][inCountry][std::stoi(inYear)].male += inData;
             }
-            else if(inData == "F"){
-                Names[inName][inCountry][inYear].female += inData;
+            else if(inGender == "F"){
+                Names[inName][inCountry][std::stoi(inYear)].female += inData;
             }
         }
     }
-
+/*
     bool end = false;
     do{
         std::string inName;
@@ -119,7 +122,7 @@ int main(){
         }
         printData(inName, likelySex, sexProb, likelyYear)
     }while(!end);
-    //Do math about it
+    //Do math about it*/
 
     //Use input stream to have user give name, search and return math
 }
